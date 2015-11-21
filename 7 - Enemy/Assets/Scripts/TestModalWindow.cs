418d8fe9083a6a3b8f2647e4deb3d8136d902bd1
Duane Rosenbaum <duane06@gmail.com>
2015-11-20 20:15:51 -0700

    Adding closing dialogue

---
+++ 7 - Enemy/Assets/Scripts/TestModalWindow.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class TestModalWindow : MonoBehaviour {
+
+	private ModalPanel modalPanel;
+	private DisplayManager displayManager;
+
+	private UnityAction myYesAction;
+	private UnityAction myNoAction;
+	private UnityAction myCancelAction;
+
+	void Awake ()
+	{
+		modalPanel = ModalPanel.Instance ();
+		displayManager = DisplayManager.Instance ();
+
+		myYesAction = new UnityAction (TestYesFunction);
+		myNoAction = new UnityAction (TestNoFunction);
+		myCancelAction = new UnityAction (TestCancelFunction);
+	}
+
+//  Send to the Modal Panel to set up the Buttons and Functions to call
+
+	public void TestYNC ()
+	{
+		modalPanel.Choice ("Would you like a poke in the eye?/nHow about with a sharp stick?", myYesAction, myNoAction, myCancelAction);
+	}
+		                                   
+//  These are wrapped into UnityActions
+
+	void TestYesFunction ()
+
+	{
+		displayManager.DisplayMessage ("Heck feah! Yup!");
+	}
+	void TestNoFunction ()
+	{
+		displayManager.DisplayMessage ("No way, Jose!");
+	}
+	void TestCancelFunction ()
+	{
+		displayManager.DisplayMessage ("I give up!");
+	}
+
+	
+	
+}
