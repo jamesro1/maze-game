c6360b5611a7eb7071c10a05b815df61da04b2e7
Duane Rosenbaum <duane06@gmail.com>
2015-12-02 10:44:41 -0700

    2015-12-02 Working Well, Pre Score

--- 7 - Enemy/Assets/Scripts/EventDB.cs
+++ 7 - Enemy/Assets/Scripts/EventDB.cs
@@ -1,28 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class EventDB
+	public static class EventDB
 {
-	public static bool isOver = false;
-
-	//Positive numbers are damage, negative numbers are heals
-	public static void TakeDamage (GameObject entity, int dmg)
-	{
-		if (entity.tag == "Player") 
+			public static bool isOver = false;
+		
+				//Positive numbers are damage, negative numbers are heals
+					public static void TakeDamage (GameObject entity, int dmg)
 		{
-			GameObject.Find ("Canvas").GetComponent<InGameHUD> ().PlayerTakesDamage(dmg);
+					if (entity.tag == "Player") 
+					{
+							GameObject.Find ("Canvas").GetComponent<InGameHUD> ().PlayerTakesDamage(dmg);
+						}
+				}
+		
+				public static void Death (GameObject gameObject)
+			{
+					if (gameObject.tag == "Enemy")
+							GameObject.Find ("Canvas").GetComponent<InGameHUD> ().EnemyDeath (gameObject);
+				}
+		
+				public static void WinCondition ()
+			{
+					GameObject.Find ("Canvas").GetComponent<TestModalWindow> ().TestYNC ();
+					isOver = true;
+				}
 		}
-	}
-
-	public static void Death (GameObject gameObject)
-	{
-		if (gameObject.tag == "Enemy")
-			GameObject.Find ("Canvas").GetComponent<InGameHUD> ().EnemyDeath (gameObject);
-	}
-
-	public static void WinCondition ()
-	{
-		GameObject.Find ("Canvas").GetComponent<TestModalWindow> ().TestYNC ();
-		isOver = true;
-	}
-}
--- 7 - Enemy/Assets/Scripts/InGameHUD.cs
+++ 7 - Enemy/Assets/Scripts/InGameHUD.cs
@@ -3,35 +3,35 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class InGameHUD : MonoBehaviour {
-	private int health = 100;
-	private int coins = 0;
-	private int score = 0;
-	public Slider healthBar;
+		private int health = 100;
+		private int coins = 0;
+		private int score = 0;
+		public Slider healthBar;
 
-	void Start()
-	{
-		EventDB.isOver = false;
+			void Start()
+		{
+				EventDB.isOver = false;
+			}
+		public void EnemyDeath(GameObject gameobject)
+		{
+				score ++;
+				Destroy (gameobject);
+
+					if (score >= 3)
+							EventDB.WinCondition ();
+			}
+
+			public void PlayerTakesDamage (int dmg)
+		{
+				healthBar.value -= dmg;
+				//Start your fade coroutine here
+			
+						if (healthBar.value == 0)
+								EventDB.WinCondition ();
+			}
+
+			//Make you color fade coroutine here
+				//Instead of color.a, use a Color.Lerp
+
+
 	}
-	public void EnemyDeath(GameObject gameobject)
-	{
-		score ++;
-		Destroy (gameobject);
-
-		if (score >= 3)
-			EventDB.WinCondition ();
-	}
-
-	public void PlayerTakesDamage (int dmg)
-	{
-		healthBar.value -= dmg;
-		//Start your fade coroutine here
-
-		if (healthBar.value == 0)
-			EventDB.WinCondition ();
-	}
-
-	//Make you color fade coroutine here
-	//Instead of color.a, use a Color.Lerp
-
-
-}
